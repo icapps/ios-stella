7fe3e21f7d8c799f5563dd423f67bd021f890a29
Jelle Vandebeeck <jelle@fousa.be>
2016-06-07 17:45:13 +0200

    Add package manager support.

---
+++ Package.swift
@@ -0,0 +1,8 @@
+import PackageDescription
+
+let package = Package(
+  name: "Stella",
+  dependencies: [
+    .Package(url: "https://github.com/icapps/ios-stella.git", majorVersion: 0, minor: 1)
+  ]
+)
