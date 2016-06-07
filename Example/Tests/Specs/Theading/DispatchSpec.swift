ce063d1b8fbfce6820234b7477a1fd57578c0cb6
Jelle Vandebeeck <jelle@fousa.be>
2016-06-07 16:06:31 +0200

    Add correct dispatch specs.

--- Example/Tests/Specs/Theading/DispatchSpec.swift
+++ Example/Tests/Specs/Theading/DispatchSpec.swift
@@ -8,13 +8,40 @@
 
 import Quick
 import Nimble
+import Stella
 
 class DispatchSpec: QuickSpec {
     override func spec() {
         
-        describe("Dispatch handling") { 
-            it("Should be performed on the main queue") {
-                expect(true).to(equal(true))
+        describe("dispatch handling") {
+            it("should be performed on the main thread") {
+                var onMainQueue = false
+                dispatch_async_main {
+                    let currentQueue = dispatch_queue_get_label(DISPATCH_CURRENT_QUEUE_LABEL)
+                    let mainQueue = dispatch_queue_get_label(dispatch_get_main_queue())
+                    onMainQueue = currentQueue == mainQueue
+                }
+                expect(onMainQueue).toEventually(beTrue(), timeout: 1)
+            }
+            
+            it("should be performed on the main thread when comming from a background thread") {
+                var onMainQueue = false
+                var onBackgroundQueue = false
+                
+                let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+                dispatch_async(queue) {
+                    let currentBackgroundQueue = dispatch_queue_get_label(DISPATCH_CURRENT_QUEUE_LABEL)
+                    let backgroundQueue = dispatch_queue_get_label(queue)
+                    onBackgroundQueue = currentBackgroundQueue == backgroundQueue
+                    
+                    dispatch_async_main {
+                        let currentQueue = dispatch_queue_get_label(DISPATCH_CURRENT_QUEUE_LABEL)
+                        let mainQueue = dispatch_queue_get_label(dispatch_get_main_queue())
+                        onMainQueue = currentQueue == mainQueue
+                    }
+                }
+                expect(onBackgroundQueue).toEventually(beTrue(), timeout: 1)
+                expect(onMainQueue).toEventually(beTrue(), timeout: 1)
             }
         }
         
