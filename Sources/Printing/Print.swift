00c8e451cb18bc3be93fa90386cab1985c5ca058
Jelle Vandebeeck <jelle@fousa.be>
2016-06-07 16:29:28 +0200

    Add documentation to the printing functions.

--- Sources/Printing/Print.swift
+++ Sources/Printing/Print.swift
@@ -8,14 +8,35 @@
 
 import Foundation
 
+/**
+ Writes the textual representations of items, prefixed with a 🍞 emoji, into the standard output.
+ 
+ This function is used to print breadcrumbs.
+ 
+ - Parameter items: The items to write to the output.
+ */
 public func printBreadcrumb(items: Any...) {
     print("🍞", items)
 }
 
+/**
+ Writes the textual representations of items, prefixed with a 🔥 emoji, into the standard output.
+ 
+ This function is used to print errors.
+ 
+ - Parameter items: The items to write to the output.
+ */
 public func printError(items: Any...) {
     print("🔥", items)
 }
 
+/**
+ Writes the textual representations of items, prefixed with a 🎯 emoji, into the standard output.
+ 
+ This function is used to print user actions.
+ 
+ - Parameter items: The items to write to the output.
+ */
 public func printAction(items: Any...) {
     print("🎯", items)
 }
