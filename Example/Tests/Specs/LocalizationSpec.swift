d813e92b57e4929ef1c300a9364e41ab67093f8a
Jelle Vandebeeck <jelle@fousa.be>
2016-06-07 16:51:54 +0200

    Add the localization code.

---
+++ Example/Tests/Specs/LocalizationSpec.swift
@@ -0,0 +1,25 @@
+//
+//  LocalizationSpec.swift
+//  Stella
+//
+//  Created by Jelle Vandebeeck on 07/06/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import Quick
+import Nimble
+import Stella
+
+class LocalizationSpec: QuickSpec {
+    override func spec() {
+        
+        describe("localization") {
+            it("localize the current string to the correct language") {
+                let key = "some_key"
+                let nativeLocalized = NSLocalizedString(key, comment: "")
+                expect(key.localizedString).to(equal(nativeLocalized))
+            }
+        }
+        
+    }
+}
