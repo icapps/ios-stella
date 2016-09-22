eb38e4b4cb10e2273f2bc139c8a1dd5387e001eb
StijnWillems <stijn.willems@icapps.com>
2016-09-22 18:00:51 +0200

    integrated prints

--- Sources/Printing/Print.swift
+++ Sources/Printing/Print.swift
@@ -8,33 +8,59 @@
 
 /**
  Writes the textual representations of items, prefixed with a 🍞 emoji, into the standard output.
- 
+
  This textual representations is used for breadcrumbs.
- 
+
  - Parameter items: The items to write to the output.
+ - Returns : text to be printed
  */
-public func printBreadcrumb(_ items: Any...) {
-    print("🍞", items.map { String(describing: $0) }.joined(separator: " "))
+@discardableResult
+public func printBreadcrumb(_ items: Any...) -> String {
+    let text = "🍞 " + items.map { String(describing: $0) }.joined(separator: " ")
+    print("\(text)")
+    return text
 }
 
 /**
  Writes the textual representations of items, prefixed with a 🔥 emoji, into the standard output.
- 
+
  This textual representations is used for errors.
- 
+
  - Parameter items: The items to write to the output.
+ - Returns : text to be printed
  */
-public func printError(_ items: Any...) {
-    print("🔥", items.map { String(describing: $0) }.joined(separator: " "))
+@discardableResult
+public func printError(_ items: Any...) -> String {
+    let text = "🔥 " + items.map { String(describing: $0) }.joined(separator: " ")
+    print(text)
+    return text
 }
 
 /**
  Writes the textual representations of items, prefixed with a 🎯 emoji, into the standard output.
- 
+
  This textual representations is used for user actions.
- 
+
  - Parameter items: The items to write to the output.
+ - Returns : text to be printed
  */
-public func printAction(_ items: Any...) {
-    print("🎯", items.map { String(describing: $0) }.joined(separator: " "))
+@discardableResult
+public func printAction(_ items: Any...) -> String {
+    let text = "🎯 " + items.map { String(describing: $0) }.joined(separator: " ")
+    print("\(text)")
+    return text
+}
+
+/**
+ Writes the textual representations of items, prefixed with a 🤔 emoji, into the standard output.
+
+ This textual representations is used for times when you want to log a text in conspicuous situations. Like when parsing and a key that is not obligatoiry is missing. You tell the developer:" You use my code but I think this is wrong."
+
+ - Parameter items: The items to write to the output.
+ - Returns : text to be printed
+ */
+public func printThought(items: Any...) -> String {
+    let text = "🤔 " + items.map { String(describing: $0) }.joined(separator: " ")
+    print("\(text)")
+    return text
 }
