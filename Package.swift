0f580bffa9c632c19f68f635c903c751a427efc3
Jelle Vandebeeck <jelle@fousa.be>
2016-09-26 10:36:27 +0200

    Version bump to 1.0.0

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
   name: "Stella",
   dependencies: [
-    .Package(url: "https://github.com/icapps/ios-stella.git", majorVersion: 0, minor: 3)
+    .Package(url: "https://github.com/icapps/ios-stella.git", majorVersion: 1, minor: 0)
   ]
 )
