0508a52034da0718196d2b26dd82b03574b63aef
Dylan Gyesbreghs <dgyesbreghs@gmail.com>
2017-11-25 12:57:07 +0100

    Update for Swift 4

--- Sources/String Size Calculation/StringSizeCalculation.swift
+++ Sources/String Size Calculation/StringSizeCalculation.swift
@@ -12,7 +12,7 @@ extension String {
 
 	public func height(constraintTo width: CGFloat, font: UIFont) -> CGFloat {
 		let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
-		let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSFontAttributeName: font], context: nil)
+		let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedStringKey.font: font], context: nil)
 
 		return boundingBox.height
 	}
