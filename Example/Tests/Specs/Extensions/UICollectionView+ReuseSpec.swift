d982bb93313c83ae4faf8c4622b0f5c7da027ea4
Jelle Vandebeeck <jelle@fousa.be>
2018-02-13 09:32:29 +0100

    Complete the generic register tests.

--- Example/Tests/Specs/Extensions/UICollectionView+ReuseSpec.swift
+++ Example/Tests/Specs/Extensions/UICollectionView+ReuseSpec.swift
@@ -21,21 +21,39 @@ private class MockedCollectionView: UICollectionView {
     private(set) var dequeuedIdentifier: String?
     private(set) var dequeuedIndexPath: IndexPath?
     
-    private(set) var dequeuedSupplementaryIdentifier: String?
-    private(set) var dequeuedSupplementaryKind: String?
-    private(set) var dequeuedSupplementaryIndexPath: IndexPath?
-    
     override func dequeueReusableCell(withReuseIdentifier identifier: String, for indexPath: IndexPath) -> UICollectionViewCell {
         dequeuedIdentifier = identifier
         dequeuedIndexPath = indexPath
         return MockedCollectionViewCell()
     }
+
+    private(set) var dequeuedSupplementaryIdentifier: String?
+    private(set) var dequeuedSupplementaryKind: String?
+    private(set) var dequeuedSupplementaryIndexPath: IndexPath?
     
     override func dequeueReusableSupplementaryView(ofKind elementKind: String, withReuseIdentifier identifier: String, for indexPath: IndexPath) -> UICollectionReusableView {
         dequeuedSupplementaryKind = elementKind
         dequeuedSupplementaryIndexPath = indexPath
         dequeuedSupplementaryIdentifier = identifier
         return MockedCollectionReusableView()
+    }
+
+    private(set) var registeredNib: UINib?
+    private(set) var registeredCellIdentifier: String?
+
+    override func register(_ nib: UINib?, forCellWithReuseIdentifier identifier: String) {
+        registeredNib = nib
+        registeredCellIdentifier = identifier
+    }
+
+    private(set) var registeredSupplementaryNib: UINib?
+    private(set) var registeredSupplementaryKind: String?
+    private(set) var registeredSupplementaryIdentifier: String?
+
+    override func register(_ nib: UINib?, forSupplementaryViewOfKind kind: String, withReuseIdentifier identifier: String) {
+        registeredSupplementaryNib = nib
+        registeredSupplementaryKind = kind
+        registeredSupplementaryIdentifier = identifier
     }
     
 }
@@ -66,6 +84,21 @@ class UICollectionViewReuseSpec: QuickSpec {
                 expect(collectionView.dequeuedSupplementaryIdentifier) == MockedCollectionReusableView.reuseIdentifier
                 expect(collectionView.dequeuedSupplementaryIndexPath) == indexPath
                 expect(collectionView.dequeuedSupplementaryKind) == "Some"
+            }
+
+            it("should register the correct cell") {
+                collectionView.register(class: MockedCollectionViewCell.self)
+
+                expect(collectionView.registeredNib).toNot(beNil())
+                expect(collectionView.registeredCellIdentifier) == MockedCollectionViewCell.reuseIdentifier
+            }
+
+            it("should register the correct supplementary view") {
+                collectionView.register(class: MockedCollectionReusableView.self, forSupplementaryViewOfKind: "Some")
+
+                expect(collectionView.registeredSupplementaryNib).toNot(beNil())
+                expect(collectionView.registeredSupplementaryKind) == "Some"
+                expect(collectionView.registeredSupplementaryIdentifier) == MockedCollectionReusableView.reuseIdentifier
             }
         }
     }
