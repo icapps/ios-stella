456095f2b068a5f5ee68638e7417646485b77b45
Jelle Vandebeeck <jelle@fousa.be>
2018-03-21 17:59:39 +0100

    Make it possible to set an access group to a keychain item.

--- Sources/Keychain/Keychain.swift
+++ Sources/Keychain/Keychain.swift
@@ -11,11 +11,14 @@ public let Keychain = KeychainHandler.shared
 
 /// The `KeychainHandler` class is responsible for the interaction with the keychain.
 public class KeychainHandler {
-    
+
+    /// Define the access group if you want to share the keychain values with other applications. When not set
+    /// this value will be ignored, and no access group will be set.
     public var accessGroupName: String?
+
     public static let shared = KeychainHandler()
     
-    fileprivate func data(for key: String) -> Data? {
+    fileprivate func data(for key: String, accessControl: SecAccessControl?) -> Data? {
         var query: [String: AnyObject] = [
             kSecClass as String      : kSecClassGenericPassword as NSString,
             kSecMatchLimit as String : kSecMatchLimitOne,
@@ -23,7 +26,9 @@ public class KeychainHandler {
             kSecAttrService as String: (Bundle.main.bundleIdentifier ?? "") as AnyObject,
             kSecAttrAccount as String: key as AnyObject
         ]
-        
+        if let accessControl = accessControl {
+            query[kSecAttrAccessControl as String] = accessControl as AnyObject
+        }
         if let accessGroupName = self.accessGroupName {
             query[kSecAttrAccessGroup as String] = accessGroupName as AnyObject
         }
@@ -31,12 +36,15 @@ public class KeychainHandler {
         return self.secItemCopy(query).data as? Data
     }
     
-    fileprivate func set(_ data: Data?, for key: String) -> Bool {
+    fileprivate func set(_ data: Data?, for key: String, accessControl: SecAccessControl?) -> Bool {
         var query: [String: AnyObject] = [
             kSecClass as String      : (kSecClassGenericPassword as NSString),
             kSecAttrAccount as String: key as AnyObject,
             kSecAttrService as String: (Bundle.main.bundleIdentifier ?? "") as AnyObject,
         ]
+        if let accessControl = accessControl {
+            query[kSecAttrAccessControl as String] = accessControl as AnyObject
+        }
         if let accessGroupName = self.accessGroupName {
             query[kSecAttrAccessGroup as String] = accessGroupName as AnyObject
         }
@@ -83,14 +91,19 @@ open class Keys {}
 /// The `Key` defines the key and the value type for a certain keychain value.
 open class Key<ValueType>: Keys {
     fileprivate let key: String
+    fileprivate let accessControl: SecAccessControl?
     
     /// Initialize the key in your `Keys` extension.
+    ///
+    /// - parameter key: The key you want to use as the unique Keychain value.
+    /// - parameter accessControl: The access control you want to use for the given key.
     ///
     /// ```
     /// static let string = Key<String?>("the string keychain key")
     /// ```
-    public init(_ key: String) {
+    public init(_ key: String, accessControl: SecAccessControl? = nil) {
         self.key = key
+        self.accessControl = accessControl
     }
 }
 
@@ -100,17 +113,22 @@ public extension KeychainHandler {
     ///
     /// ```
     /// static let string = Key<String?>("the string defaults key")
+    ///
+    /// let accessControl = SecAccessControlCreateWithFlags(kCFAllocatorDefault,
+    ///                                                     kSecAttrAccessibleWhenPasscodeSetThisDeviceOnly,
+    ///                                                     .userPresence, &error)
+    /// static let noBackup = Key<String?>("not in backup", accessControl)
     /// ```
     public subscript(key: Key<String?>) -> String? {
         get {
-            if let data = data(for: key.key) {
+            if let data = data(for: key.key, accessControl: key.accessControl) {
                 return String(data: data, encoding: .utf8)
             }
             return nil
         }
         set {
             let value = newValue?.data(using: .utf8, allowLossyConversion: false)
-            let _ = set(value, for: key.key)
+            let _ = set(value, for: key.key, accessControl: key.accessControl)
         }
     }
 }
