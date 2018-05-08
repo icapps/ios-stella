7131d5221c582adfe2a515cbe181d2e634bb283a
Jelle Vandebeeck <jelle@fousa.be>
2018-05-08 12:49:56 +0200

    Typo.

--- Sources/Extensions/UIView+Responder.swift
+++ Sources/Extensions/UIView+Responder.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 extension UIView {
-    /// Return the controller that is manages this view.
+    /// Return the controller that is responsible for managing this view.
     public var respondingController: UIViewController? {
         if let controller = next as? UIViewController { return controller }
         if let view = next as? UIView { return view.respondingController }
