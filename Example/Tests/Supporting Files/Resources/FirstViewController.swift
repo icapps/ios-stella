bc332916e89956308a75449cd7b25872eeeba64f
Jelle Vandebeeck <jelle@fousa.be>
2018-11-28 17:37:54 +0100

    Add the controller from storyboard functionality.

---
+++ Example/Tests/Supporting Files/Resources/FirstViewController.swift
@@ -0,0 +1,12 @@
+//
+//  FirstViewController.swift
+//  Tests
+//
+//  Created by Jelle Vandebeeck on 28/11/2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+}
