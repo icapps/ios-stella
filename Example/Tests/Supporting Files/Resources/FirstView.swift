dc8c7d0590bdbe1b559fde344ff7d64f3d441682
Jelle Vandebeeck <jelle@fousa.be>
2018-11-28 17:57:59 +0100

    Add the load view from nib functionality.

---
+++ Example/Tests/Supporting Files/Resources/FirstView.swift
@@ -0,0 +1,12 @@
+//
+//  FirstView.swift
+//  Tests
+//
+//  Created by Jelle Vandebeeck on 28/11/2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class FirstView: UIView {
+}
--- Sources/Extensions/UIView+Nib.swift
+++ Sources/Extensions/UIView+Nib.swift
@@ -22,4 +22,14 @@ extension UIView {
     public class var nib: UINib {
         return UINib(nibName: nibName, bundle: Bundle(for: self))
     }
+
+    /// Load the custom view from the nib with the same name in the same bundle.
+    ///
+    /// In order to use this function, make sure that you already types the destination variable:
+    /// ex: `let view: SomeView = UIView.loadFromNib()`
+    public class func loadFromNib<View: UIView>() -> View {
+        let views = View.nib.instantiate(withOwner: nil, options: nil)
+        // swiftlint:disable force_cast
+        return views.first as! View
+    }
 }
