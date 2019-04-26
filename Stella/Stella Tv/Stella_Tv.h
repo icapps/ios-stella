2e91ba7ead7865bc776d57ec4646cf49a157cc45
Jelle Vandebeeck <jelle@fousa.be>
2019-04-26 18:40:35 +0200

    Add the tvOS target + scheme.

---
+++ Stella/Stella Tv/Stella_Tv.h
@@ -0,0 +1,19 @@
+//
+//  Stella_Tv.h
+//  Stella Tv
+//
+//  Created by Jelle Vandebeeck on 26/04/2019.
+//  Copyright © 2019 iCapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Stella_Tv.
+FOUNDATION_EXPORT double Stella_TvVersionNumber;
+
+//! Project version string for Stella_Tv.
+FOUNDATION_EXPORT const unsigned char Stella_TvVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Stella_Tv/PublicHeader.h>
+
+
