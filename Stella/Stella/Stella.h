83cde2d80050c507170055b1a4348ff6df131fe5
Jelle Vandebeeck <jelle@fousa.be>
2019-04-26 18:24:54 +0200

    Create a dedicated Stella project inside the workspace.

---
+++ Stella/Stella/Stella.h
@@ -0,0 +1,19 @@
+//
+//  Stella.h
+//  Stella
+//
+//  Created by Jelle Vandebeeck on 26/04/2019.
+//  Copyright © 2019 iCapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Stella.
+FOUNDATION_EXPORT double StellaVersionNumber;
+
+//! Project version string for Stella.
+FOUNDATION_EXPORT const unsigned char StellaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Stella/PublicHeader.h>
+
+
