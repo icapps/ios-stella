29b9b5ffc6492d36fb3579037e5df84e8d3910a5
Jelle Vandebeeck <jelle@fousa.be>
2019-06-05 15:55:22 +0200

    Update the package with versions.

--- Package.swift
+++ Package.swift
@@ -3,11 +3,17 @@ import PackageDescription
 
 let package = Package(
   name: "Stella",
+  platforms: [
+      .macOS(.v10_10),
+      .iOS(.v9),
+      .tvOS(.v9)
+  ],
   products: [
     .library(name: "Stella", targets: ["Stella"])
   ],
   dependencies: [],
   targets: [
-        .target(name: "Stella", dependencies: [], path: "Sources")
-    ]
+    .target(name: "Stella", dependencies: [], path: "Sources")
+  ],
+  swiftLanguageVersions: [.v4_2]
 )
