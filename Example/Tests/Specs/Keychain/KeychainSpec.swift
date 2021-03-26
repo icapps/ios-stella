380f696478178b25920f7e4087d3300a221ed121
Joris Timmerman <joris@hyperspacebar.com>
2021-03-26 11:14:54 +0100

    Added tests for Keychain

--- Example/Tests/Specs/Keychain/KeychainSpec.swift
+++ Example/Tests/Specs/Keychain/KeychainSpec.swift
@@ -16,6 +16,15 @@ extension Keys {
     static let stringValue = Key<String?>("stringValue")
 }
 
+private struct TestCodable: Codable, Equatable {
+    var id: String
+    var timestamp: Date
+    init(id: String, timestamp: Date) {
+        self.id = id
+        self.timestamp = timestamp
+    }
+}
+
 class KeychainSpec: QuickSpec {
     override func spec() {
         describe("keychain") {
@@ -38,6 +47,34 @@ class KeychainSpec: QuickSpec {
                 it("should be able to read to the keychain") {
                     _ = Keychain.save("Some string value", forKey: "stringValue")
                     expect(Keychain[.stringValue]).to(equal("Some string value"))
+                }
+
+                it("should be able to write a codable value to the keychain") {
+                    let value = TestCodable(id: "123",
+                                            timestamp: Date(timeIntervalSince1970: 2000))
+                    keychain[.testKey] = value
+                    expect(keychain[.testKey]) == value
+                }
+
+                it("Should override a codable value to the keychain") {
+                    let value1 = TestCodable(id: "123",
+                                             timestamp: Date(timeIntervalSince1970: 2000))
+                    let value2 = TestCodable(id: "456",
+                                             timestamp: Date(timeIntervalSince1970: 3000))
+                    keychain[.testKey] = value1
+                    keychain[.testKey] = value2
+
+                    expect(keychain[.testKey]) == value2
+                }
+
+                it("should be able to delete a codable value from the keychain") {
+                    let value = TestCodable(id: "123",
+                                            timestamp: Date(timeIntervalSince1970: 2000))
+                    keychain[.testKey] = value
+                    expect(keychain[.testKey]).toNot(beNil())
+
+                    keychain[.testKey] = nil
+                    expect(keychain[.testKey]).to(beNil())
                 }
             }
         }
